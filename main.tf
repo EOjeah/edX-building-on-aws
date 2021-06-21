5605798c86d76e3c6b367cb3d5192022501598e5
Emmanuel Ojeah <36337836+EOjeah@users.noreply.github.com>
2021-06-22 00:06:03 +0100

    ssh from anywhere

--- main.tf
+++ main.tf
@@ -25,6 +25,14 @@ resource "aws_security_group" "web-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
